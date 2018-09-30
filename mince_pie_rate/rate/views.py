221629d3f3a0d8dd2b2894db4fab1e02c0ec8387
Matthew Champion <mattjameschampion@gmail.com>
2018-09-30 13:37:46 +0100

    Add a review view

--- mince_pie_rate/rate/views.py
+++ mince_pie_rate/rate/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def index(request):
     return HttpResponse("Welcome to Mince Pie-Rate. Yarr!")
+
+def review(request):
+    return HttpResponse("Review a mince-pie.")
