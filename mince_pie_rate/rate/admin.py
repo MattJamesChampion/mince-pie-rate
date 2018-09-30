f31fe145a81bb071b2b1e083690934ecc70d7b84
Matthew Champion <mattjameschampion@gmail.com>
2018-09-30 12:02:23 +0100

    Add MincePie and Review models

--- mince_pie_rate/rate/admin.py
+++ mince_pie_rate/rate/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import MincePie, Review
+
+
+admin.site.register(MincePie)
+admin.site.register(Review)
--- mince_pie_rate/rate/models.py
+++ mince_pie_rate/rate/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+
+class MincePie(models.Model):
+    brand = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return "{0} - {1}".format(self.brand, self.name)
+
+class Review(models.Model):
+    mince_pie = models.ForeignKey(MincePie, on_delete=models.CASCADE)
+    rating = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(10)])
+
+    def __str__(self):
+        return "{0}: {1}".format(str(self.mince_pie), self.rating)
