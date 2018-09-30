8429513f4c0133f62df80b0ab3abeb8f656290ba
Matthew Champion <mattjameschampion@gmail.com>
2018-09-30 15:49:14 +0100

    Add a list to the index view of all of the mince pies in the database

--- mince_pie_rate/rate/views.py
+++ mince_pie_rate/rate/views.py
@@ -1,9 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import MincePie
+
 
 def index(request):
-    return render(request, 'rate/index.html')
+    mince_pie_list = MincePie.objects.all()
+    context = {
+        'mince_pie_list' : mince_pie_list
+    }
+    return render(request, 'rate/index.html', context)
 
 def review(request):
     return render(request, 'rate/review.html')
