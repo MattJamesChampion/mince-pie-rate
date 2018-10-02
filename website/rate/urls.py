788e305824d6f65927a3d4e3f838303d434ce089
Matthew Champion <mattjameschampion@gmail.com>
2018-10-02 23:25:45 +0100

    Add basic implementation of review functionality for the detail view

--- website/rate/urls.py
+++ website/rate/urls.py
@@ -6,4 +6,5 @@ app_name = 'rate'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:mince_pie_id>', views.detail, name='detail'),
+    path('<int:mince_pie_id>/review/', views.review, name='review'),
 ]
--- website/rate/views.py
+++ website/rate/views.py
@@ -1,7 +1,8 @@
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 
-from .models import MincePie
+from .models import MincePie, Review
 
 
 def index(request):
@@ -11,6 +12,17 @@ def index(request):
     }
     return render(request, 'rate/index.html', context)
 
+def review(request, mince_pie_id):
+    mince_pie_instance = get_object_or_404(MincePie, pk=mince_pie_id)
+    try:
+        numeric_rating = int(request.POST['numeric_rating'])
+        review_instance = Review(mince_pie=mince_pie_instance, rating=numeric_rating)
+        review_instance.save()
+    except Exception:
+        error_message = 'Error - Could not create the review'
+        return render(request, 'rate/detail.html', {'mince_pie' : mince_pie_instance, 'error_message' : error_message})
+    else:
+        return HttpResponseRedirect(reverse('rate:detail', args=(mince_pie_instance.pk,)))
 
 def detail(request, mince_pie_id):
     mince_pie = get_object_or_404(MincePie, pk=mince_pie_id)
