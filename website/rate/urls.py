27627682d2be64cd030e5ec32235c4d4bb3a9d7a
Matthew Champion <mattjameschampion@gmail.com>
2018-10-04 00:17:22 +0100

    Create the add page that allows for mince pies to be created from the UI

--- website/rate/urls.py
+++ website/rate/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('mince-pies/', views.overview, name='overview'),
     path('mince-pies/<int:mince_pie_id>', views.detail, name='detail'),
     path('mince-pies/<int:mince_pie_id>/review/', views.review, name='review'),
+    path('mince-pies/add/', views.add, name='add'),
+    path('mince-pies/add/submit/', views.submit_add, name='submit_add'),
 ]
--- website/rate/views.py
+++ website/rate/views.py
@@ -37,3 +37,22 @@ def review(request, mince_pie_id):
 def detail(request, mince_pie_id):
     mince_pie = get_object_or_404(MincePie, pk=mince_pie_id)
     return render(request, 'rate/detail.html', {'mince_pie': mince_pie})
+
+def add(request):
+    context = {
+        'active_page' : 'add',
+    }
+    return render(request, 'rate/add.html', context)
+
+def submit_add(request):
+    try:
+        brand = request.POST['brand']
+        name = request.POST['name']
+        mince_pie_instance = MincePie(brand=brand, name=name, created_by=request.user)
+        mince_pie_instance.save()
+    except Exception:
+        error_message = 'Error - Could not create the mince pie'
+        return render(request, 'rate/add.html', {'error_message' : error_message})
+    else:
+        return HttpResponseRedirect(reverse('rate:detail', args=(mince_pie_instance.pk,)))
+        
