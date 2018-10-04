5d7f0d8c83f628d1f57e003eb72e30773fa91ed5
Matthew Champion <mattjameschampion@gmail.com>
2018-10-04 01:18:29 +0100

    Add local_settings_template

---
+++ website/mince_pie_rate/local_settings_template.py
@@ -0,0 +1,23 @@
+import os
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = ''
+
+SECRET_KEY = ''
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+# Database
+# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': '',
+        'NAME': '',
+    }
+}
+
+LANGUAGE_CODE = ''
+
+TIME_ZONE = ''
