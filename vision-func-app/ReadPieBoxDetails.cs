f81267a0771c300dd72fffa87af133fe5a64d3f9
Michael Wade <spikewade@gmail.com>
2018-10-06 09:53:30 +0100

    Initial comit

---
+++ vision-func-app/ReadPieBoxDetails.cs
@@ -0,0 +1,68 @@
+
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
+
+namespace mincepierate.Vision
+{
+    public static class ReadPieBoxDetails
+    {
+        private const string subscriptionKey = "";
+        private const int numberOfCharsInOperationId = 36;
+
+        [FunctionName("ReadPieBoxDetails")]
+        public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequest req, ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+            ComputerVisionClient computerVision = new ComputerVisionClient(
+                new ApiKeyServiceClientCredentials(subscriptionKey),
+                new System.Net.Http.DelegatingHandler[] { });
+                
+            var imageUrl = req.Query["imageUrl"];
+            var textHeaders = await computerVision.RecognizeTextAsync(
+                    imageUrl, TextRecognitionMode.Printed);
+            
+            //string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            //dynamic data = JsonConvert.DeserializeObject(requestBody);
+            
+            return (ActionResult)new OkObjectResult(GetTextAsync(computerVision,textHeaders.OperationLocation));
+        }
+
+        private static async Task<string> GetTextAsync(
+            ComputerVisionClient computerVision, string operationLocation)
+        {
+            // Retrieve the URI where the recognized text will be
+            // stored from the Operation-Location header
+            string operationId = operationLocation.Substring(
+                operationLocation.Length - numberOfCharsInOperationId);
+
+            Console.WriteLine("\nCalling GetHandwritingRecognitionOperationResultAsync()");
+            TextOperationResult result =
+                await computerVision.GetTextOperationResultAsync(operationId);
+
+            // Wait for the operation to complete
+            int i = 0;
+            int maxRetries = 10;
+            while ((result.Status == TextOperationStatusCodes.Running ||
+                    result.Status == TextOperationStatusCodes.NotStarted) && i++ < maxRetries)
+            {
+                Console.WriteLine(
+                    "Server status: {0}, waiting {1} seconds...", result.Status, i);
+                await Task.Delay(1000);
+
+                result = await computerVision.GetTextOperationResultAsync(operationId);
+            }
+
+            return string.Join(Environment.NewLine, result.RecognitionResult.Lines);
+        }
+    }
+}
